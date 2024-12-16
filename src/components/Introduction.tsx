4e95912f67d354abfc3bcd1798f4af450c4f46fb
Salmaan Mushtaq <salman@imobisoft.co.uk>
2024-12-16 12:50:31 +0530

    unused code commited

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import MySelf from "../assets/images/myself.webp";
 import TypingAnimation from "./ui/typing-animation";
 function Introduction() {
