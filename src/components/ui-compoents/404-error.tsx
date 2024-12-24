68cbbe55dacfb43680883ef2e3b5a422c6ee41db
Salmaan Mushtaq <salman@imobisoft.co.uk>
2024-12-24 10:01:26 +0530

    404 added

--- src/components/ui-compoents/404-error.tsx
+++ src/components/ui-compoents/404-error.tsx
@@ -1,18 +1,23 @@
 "use client";
-import { WavyBackground } from "../ui/wavy-background";
+
+import { Link } from "@tanstack/react-router";
 
 export default function Error404() {
   return (
-    <WavyBackground
-      backgroundFill="transparent"
-      className="max-w-4xl mx-auto pb-40"
-    >
+    <section className="flex flex-col items-center justify-center h-full space-y-2">
       <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
         404 Not Found
       </p>
       <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
         Whoops! That page doesn’t exist.
       </p>
-    </WavyBackground>
+      <Link
+        to="/"
+        className="dark:text-white border-2 border-navLinkBorder customShadow p-1 px-6 bg-background rounded-full transition-all duration-200
+              hover:border-white hover:border-2"
+      >
+        Go back home
+      </Link>
+    </section>
   );
 }
